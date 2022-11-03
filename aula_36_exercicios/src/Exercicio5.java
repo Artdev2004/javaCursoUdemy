8d7a0c2fd0529a9f9062d3289284b8baf6f8df65
Arthur <arthurcalazansn@gmail.com>
2022-11-03 14:12:28 -0300

    Adicionando Exercicio 5

---
+++ aula_36_exercicios/src/Exercicio5.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int cod, qtd;
+		double prc = 0.0;
+		System.out.println("DIGITE O CÓDIGO DO PRODUTO: ");
+		cod = sc.nextInt();
+		System.out.println("DIGITE A QUANTIDADE DO PRODUTO");
+		qtd = sc.nextInt();
+		
+		if(cod == 1) {
+			prc = qtd * 4.0;
+		}
+		else if (cod == 2) {
+			prc = qtd * 4.5;
+		}
+		else if (cod == 3) {
+			prc = qtd * 5.0;
+		}
+		else if (cod == 4) {
+			prc = qtd * 2.0;
+		}
+		else if (cod == 5) {
+			prc = qtd * 1.5;
+		}
+		
+		System.out.printf("TOTAL: R$ %.2f", prc);
+		sc.close();
+		
+	}
+
+}
