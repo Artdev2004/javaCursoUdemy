036bb336ead9c2966c29b35a2d61663bf84af86e
Artdev2004 <100302696+Artdev2004@users.noreply.github.com>
2023-02-04 18:52:29 -0300

    Adicionando Exercicio3 aula_48

---
+++ aula_48_exercicios/src/Exercicio3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int all = 0, gas = 0, die = 0, cd;
+		
+		cd = sc.nextInt();
+		
+		while(cd != 4) {
+			while(cd < 1 || cd > 4) {
+				cd = sc.nextInt();
+			}
+			if(cd == 1) {
+				all = all + 1;
+			}
+			else if(cd ==  2) {
+				gas = gas + 1;
+ 			}
+			else if(cd == 3) {
+				die = die +1;
+			}
+			cd = sc.nextInt();
+		}
+		System.out.println("MUITO OBRIGADO\n Alcool: " + all + "\n Gasolina: "
+				+ gas + "\n Diesel: " + die);
+		
+		
+
+	}
+
+}
