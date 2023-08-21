10fcff02dfdf7e4f024d5eaa82951f0a4df37cbd
Arthur S. Nascimento <arthur.soares.nascimento31@gmail.com>
2023-08-21 18:49:27 -0300

    Subindo aula 63 método área e largura

--- aula_63/src/application/Main.java
+++ aula_63/src/application/Main.java
@@ -22,18 +22,19 @@ public class Main {
 			y.a = sc.nextDouble();
 			y.b= sc.nextDouble();
 			y.c= sc.nextDouble();
-			double p = (x.a + x.b + x.c) / 2.0;
-			double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-			p = (y.a + y.b + y.c) / 2.0;
-			double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
-			System.out.printf("Triangle X area: %.4f%n", areaX);
-			System.out.printf("Triangle Y area: %.4f%n", areaY);
-			if (areaX > areaY) {
-			System.out.println("Larger area: X");
-			}
-			else {
-			System.out.println("Larger area: Y");
-			}
+			
+			
+//			double p = (x.a + x.b + x.c) / 2.0;
+//			double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+//			p = (y.a + y.b + y.c) / 2.0;
+//			double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+			System.out.printf("Triangle X area: %.2f", x.calculateArea());
+			System.out.printf("\nTriangle Y area: %.2f\n", y.calculateArea());
+			
+			Triangle.largerArea(x.calculateArea(), y.calculateArea());
+			
+			
+			
 			sc.close();
 
 		}
--- aula_63/src/entities/Triangle.java
+++ aula_63/src/entities/Triangle.java
@@ -5,4 +5,28 @@ public class Triangle {
 	public double a;
 	public double b;
 	public double c;
+	
+	
+	public double calculateArea() {
+		
+		double p, area;
+		
+		p = (a + b + c) / 2;
+		
+		area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		
+		return area;
+		
+	}
+	
+	public static void largerArea( double _areaX, double _areaY) {
+		if(_areaX > _areaY) {
+			System.out.println("Larger Area: X");
+		}else if (_areaX == _areaY){
+			System.out.println("Larger Area: Y and X");
+			
+		}else {
+			System.out.println("Larger Area: Y");
+		}
+	}
 }
